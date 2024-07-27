9cfd979bbbd596d79e0d252ed4ceb00978e94547
Abhyuday Rai <abhyudayrai03@gmail.com>
2024-07-28 00:52:01 +0530

    html page is connected and working

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
-    console.log(req.method);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello, World!\n');
+
+if(req.method === 'GET' && req.url === '/'){
+    const indexFile = path.join(__dirname, 'Frontend/static/index.html');
+    fs.readFile(indexFile, (err, data)=>{
+        if(err){
+            res.writeHead(500), {'Content-Type':'text/plain'}
+            res.end('Some internal server error');
+            return;
+        }
+        res.writeHead(200, {'Content-Type':'text/html'});
+        res.end(data);
+    })
+}
+else {
+    res.writeHead(404, 
+        {'Content-Type':'text/plain'}
+    )
+    res.end('Not Found');
+}
 });
 
 const port = 3000;
