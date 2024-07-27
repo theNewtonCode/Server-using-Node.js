30a797fa0cc75acea7c637bc03f51b7b13da544c
Abhyuday Rai <abhyudayrai03@gmail.com>
2024-07-28 01:36:52 +0530

    Variable url, for a specific registered user works

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
-const { timeStamp } = require('console');
+const url  = require('url');
+
 
 const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    const pathname = parsedUrl.pathname; 
 
 //endpoint for serving homepage on get request
 if(req.method === 'GET' && req.url === '/'){
@@ -18,6 +21,28 @@ if(req.method === 'GET' && req.url === '/'){
         res.end(data);
     })
 }
+
+//endpoint for variable url
+else if (req.method === 'GET' && pathname.startsWith('/users/')) {
+    const username = pathname.split('/')[2];
+    const usersdata = path.join(__dirname, 'Backend/users.txt');
+
+    fs.readFile(usersdata, 'utf8', (err, data)=>
+    {
+        if(err){
+            res.writeHead(500, {'Content-Type':'text/plain'});
+            res.end('Server Error');
+        }
+        const users = data.split('\n').map(line => line.trim());
+        if (users.includes(username)) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(`<h1>Hello, ${username}!</h1><p>Welcome to your personalized page.</p>`);
+        } else {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.end('<h1>User Not Found</h1><p>The username you are looking for does not exist.</p>');
+        }
+    })
+} 
 //endpoint for api request
 else if(req.method==='GET' && req.url === '/api/data'){
     const apidata = path.join(__dirname, 'Frontend/static/api data/text.txt');
